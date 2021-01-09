10b3aae8d7b98b7206d07837c0f32ef9932d54eb
syuhada-ramadhan <suhadahada18@gmail.com>
2021-01-09 15:29:57 +0700

    latihan Mencari nilai Terendah pada JavaScript selesai

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// array nomor
+const nomor = [1, 4, 5, 8, 10, 30, 50, 100];
+
+// cek nilai terendah pada array nomor
+const nilaiTerendah = Math.min(...nomor);
+
+// cetak variabel nilaiTerendah
+console.log(nilaiTerendah);
